4a32541fd03f5bc94b642edaf102ba74072ef01c
Youssef <yg9552753@gmail.com>
2024-05-18 05:03:39 +0300

    Adding the emailjs service and finishing up

--- main.js
+++ main.js
@@ -16,5 +16,29 @@ navLinks.forEach(link => {
             link.classList.remove('active');
         })
         link.classList.add('active');
+        navbar.classList.remove('mobile');
     })
 })
+
+const btn = document.querySelector('button');
+
+document.querySelector('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_sl740ds';
+   const templateID = 'template_9zbykb3';
+ 
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Sending...';
+      alert('Thanks, I have received your message!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
